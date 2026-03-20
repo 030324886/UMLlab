a02309b15c54e9054860f7fca7ad50b8905e7bb0
030324886 <2592803243@qq.com>
2026-03-20 14:56:11 +0800

    Change output student ID from 002 to 004

--- stu.java
+++ stu.java
@@ -1,6 +1,6 @@
 public class stu {
     public static void main(String[] args) {
-        System.out.println("我是学生002");
+        System.out.println("我是学生004");
         System.out.println("7778899");
     }
 }
