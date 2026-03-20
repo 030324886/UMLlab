79fabeadee2f8845a92f186e82504b907b0c6dcc
“030324886” <2592803243@qq.com>
2026-03-20 14:51:03 +0800

    第一次提交

---
+++ teacher.java
@@ -0,0 +1,7 @@
+public class stu {
+    public static void main(String[] args) {
+        System.out.println("我是教师001");
+        // 核心输出语句2：Git版本测试标记
+        System.out.println("11223344");
+    }
+}
