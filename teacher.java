7c234dd7e644def521ce801b69141b863b0ca326
“030324886” <2592803243@qq.com>
2026-03-20 15:00:21 +0800

    teacher.java11223344改为6677

--- teacher.java
+++ teacher.java
@@ -2,6 +2,6 @@ public class stu {
     public static void main(String[] args) {
         System.out.println("我是教师001");
         // 核心输出语句2：Git版本测试标记
-        System.out.println("11223344");
+        System.out.println("6677");
     }
 }
